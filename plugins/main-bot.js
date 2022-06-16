d9de2f3219dbd63cedad14b905f63a5b559ecffe
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-16 15:44:47 +0700

    Create main-bot.js

---
+++ plugins/main-bot.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/ec26801072b1794c752a9.png', m, { packname: "zifabotz", author: "@rozi" })
+}
+
+handler.customPrefix = /^(bot|woi|Cok|ngab|tod)$/i
+handler.command = new RegExp
+
+module.exports = handler
