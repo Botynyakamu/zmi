95ee43dd14acd8017d4dbdc7573bb91c9a8338f5
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-16 15:45:47 +0700

    Update main-bot.js

--- plugins/main-bot.js
+++ plugins/main-bot.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/ec26801072b1794c752a9.png', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/5fc650798a93b835ef7e9.png', m, { packname: "zifabotz", author: "@rozi" })
 }
 
 handler.customPrefix = /^(bot|woi|Cok|ngab|tod)$/i
