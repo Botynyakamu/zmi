94f83ad040a1b44ebfe08bb6fa7ca55ead4490d3
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-16 16:11:45 +0700

    Delete main-sange.js

--- main-sange.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/298c47c103f0bca3c1369.png', m, { packname: "zifabotz", author: "@rozi" })
-}
-
-handler.customPrefix = /^(kontol|ngentod|ngentot|memek|pepek|peler)$/i
-handler.command = new RegExp
-
-module.exports = handler
